374538d9d5b3dd41ae1d2ec276d234c9db6e7618
Rouse, Michael W. (S&amp;t-Student) <mwrb7d@mst.edu>
2017-11-01 14:35:39 -0500

    JSON Formatter

---
+++ Formatter/json_formatter.py
@@ -0,0 +1,27 @@
+import json
+
+import Formatter
+import Config
+
+cfg = Config.Get()
+
+
+@Formatter.Register("json")
+def json_formatter(components):
+  """ Formats the component list as JSON """
+  columns = cfg['columns']
+
+  newList = [] # New list of only dictionaries with column attributes to marshall
+
+  for component in components:
+    newComp = {}
+
+    for column in columns:
+      try:
+        newComp[column] = component[column]
+      except:
+        newComp[column] = "[unkown]"
+
+    newList.append(newComp)
+
+  return json.dumps(newList)

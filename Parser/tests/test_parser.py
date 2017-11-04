9ac4a436dc458b49f27725bf43a5348383cc3743
Rouse, Michael W. (S&amp;t-Student) <mwrb7d@mst.edu>
2017-11-04 10:14:14 -0500

    Changed unit tests for parsing netlist file

--- Parser/tests/test_parser.py
+++ Parser/tests/test_parser.py
@@ -8,10 +8,10 @@ import Parser
 # Folder for testing filess
 folder = os.path.dirname(os.path.abspath(__file__)) + "/files"
 
-single_simple_component = "{0}/single_simple_component.sch".format(folder) # File with only a single component in it
-single_complex_component = "{0}/single_complex_component.sch".format(folder)
-multiple_components = "{0}/multiple_components.sch".format(folder)
-multiple_components_duplicates = "{0}/multiple_components_duplicates.sch".format(folder)
+single_simple_component = "{0}/single_simple_component.xml".format(folder) # File with only a single component in it
+single_complex_component = "{0}/single_complex_component.xml".format(folder)
+multiple_components = "{0}/multiple_components.xml".format(folder)
+multiple_components_duplicates = "{0}/multiple_components_duplicates.xml".format(folder)
 
 
 def test_parse_simple_component_single_file():
@@ -27,7 +27,7 @@ def test_parse_simple_component_single_file():
   })
 
   # Act
-  components_found = Parser.GetComponentsFromFiles([single_simple_component])
+  components_found = Parser.GetComponentsFromNetlist(single_simple_component)
 
   # Assert
   assert len(components_found) == 1
@@ -49,12 +49,11 @@ def test_parse_complex_component_single_file():
   })
 
   # Act
-  components_found = Parser.GetComponentsFromFiles([single_complex_component])
+  components_found = Parser.GetComponentsFromNetlist(single_complex_component)
 
   # Assert
   assert len(components_found) == 1
   assert components_found[0] == expected_component, str(components_found[0]) + " != " + str(expected_component)
-
 
 
 def test_parse_multiple_components_single_file():
@@ -85,7 +84,7 @@ def test_parse_multiple_components_single_file():
   ])
 
   # Act
-  components_found = Parser.GetComponentsFromFiles([multiple_components])
+  components_found = Parser.GetComponentsFromNetlist(multiple_components)
 
   # Assert
   assert len(components_found) == len(expected_components)
@@ -122,48 +121,10 @@ def test_parse_multiple_duplicate_components_single_file():
   ])
 
   # Act
-  components_found = Parser.GetComponentsFromFiles([multiple_components_duplicates])
+  components_found = Parser.GetComponentsFromNetlist(multiple_components_duplicates)
 
   # Assert
   assert len(components_found) == len(expected_components)
 
   for i in range(0, len(components_found)):
     assert components_found[i] == expected_components[i], str(components_found[i]) + " != " + str(expected_components[i])
-
-
-def test_multiple_files():
-  """ Tests that the parser can return multiple components (with duplicates) from multiple files """
-  # Arrange
-  expected_components = KiCadComponentList([
-    KiCadComponent({
-      "name": "Component1",
-      "reference": "A1, A4, A7",
-      "value": "value1",
-      "footprint": "footprint-lib:footprint",
-      "quantity": 3
-    }),
-    KiCadComponent({
-      "name": "Component2",
-      "reference": "A2, A5",
-      "value": "value2",
-      "footprint": "footprint-lib:footprint",
-      "quantity": 2
-    }),
-    KiCadComponent({
-      "name": "Component3",
-      "reference": "A3, A6",
-      "value": "value3",
-      "footprint": "footprint-lib:footprint",
-      "quantity": 2
-    })
-  ])
-
-  # Act
-  components_found = Parser.GetComponentsFromFiles([multiple_components, multiple_components_duplicates])
-
-  # Assert
-  assert len(components_found) == len(expected_components)
-
-  for i in range(0, len(components_found)):
-    assert components_found[i] == expected_components[i], str(components_found[i]) + " != " + str(expected_components[i])
-
